f0f35783b5834ddf2887f8fae6113803a0a76369
ArthurSanzSeleme <133713885+ArthurSanzSeleme@users.noreply.github.com>
2023-09-16 19:37:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ noseX=0;
 noseY=0;
 
 function preload() {
-  clownNose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+  clownNose = loadImage('https://i.postimg.cc/Jh58zwyc/bigodon-removebg-preview.png');
 }
 
 function setup() {
@@ -25,13 +25,14 @@ function gotPoses(results)
   if(results.length > 0)
   {
   console.log(results);
-  console.log("nose x =" + results[0].pose.nose.x);
-  console.log("nose y =" + results[0].pose.nose.y);
+  noseX = results[0].pose.nose.x
+  noseY = results[0].pose.nose.y
 }
 }
 
 function draw() {
 image(video, 0, 0, 300, 300);
+image(clownNose, noseX-15, noseY-15,75, 75);
 }
 
 function takeSnapshot(){    
